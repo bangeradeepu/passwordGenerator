4684ab2cb2cabe47e2dc5567439c17175398908e
bangeradeepu <deepu.program@gmail.com>
2024-05-24 14:46:21 +0530

    newCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,50 @@
-import React, { useState, useEffect } from "react";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Button from "@mui/material/Button";
-import { Typography } from "@mui/material";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  Checkbox,
+  FormControlLabel,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+  Typography,
+  TextField,
+  InputAdornment
+} from '@mui/material';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const App = () => {
-
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-  
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
-
-
-
-
   const [includeNumbers, setIncludeNumbers] = useState(true);
   const [includeAlphabets, setIncludeAlphabets] = useState(true);
   const [includeSpecialChars, setIncludeSpecialChars] = useState(true);
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState('');
   const [passwordHistory, setPasswordHistory] = useState([]);
 
   useEffect(() => {
-    const storedPasswords =
-      JSON.parse(localStorage.getItem("passwordHistory")) || [];
+    const storedPasswords = JSON.parse(localStorage.getItem('passwordHistory')) || [];
     setPasswordHistory(storedPasswords);
   }, []);
 
   const generatePassword = () => {
-    const numbers = "0123456789";
-    const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+    const numbers = '0123456789';
+    const alphabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
 
-    let characters = "";
+    let characters = '';
     if (includeNumbers) characters += numbers;
     if (includeAlphabets) characters += alphabets;
     if (includeSpecialChars) characters += specialChars;
 
     if (characters.length === 0) {
-      alert("Please select at least one option!");
+      alert('Please select at least one option!');
       return;
     }
 
-    let generatedPassword = "";
+    let generatedPassword = '';
     for (let i = 0; i < 12; i++) {
-      generatedPassword += characters.charAt(
-        Math.floor(Math.random() * characters.length)
-      );
+      generatedPassword += characters.charAt(Math.floor(Math.random() * characters.length));
     }
 
     setPassword(generatedPassword);
@@ -74,77 +54,79 @@ const App = () => {
   const updatePasswordHistory = (newPassword) => {
     const newHistory = [newPassword, ...passwordHistory].slice(0, 5);
     setPasswordHistory(newHistory);
-    localStorage.setItem("passwordHistory", JSON.stringify(newHistory));
+    localStorage.setItem('passwordHistory', JSON.stringify(newHistory));
   };
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(password);
-    alert("Password copied to clipboard!");
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+    alert('Password copied to clipboard!');
   };
 
   return (
-    <div>
-      <Typography variant="h4" component="h4">
-        Password Generator
-      </Typography>
+    <Container maxWidth="sm">
+      <Typography variant="h4" gutterBottom>Password Generator</Typography>
       <div>
-        <FormGroup style={{ marginTop: 10 }}>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={includeNumbers}
-                onChange={(e) => setIncludeNumbers(e.target.checked)}
-              />
-            }
-            label="Include Numbers"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={includeAlphabets}
-                onChange={(e) => setIncludeAlphabets(e.target.checked)}
-              />
-            }
-            label="Include Alphabets"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={includeSpecialChars}
-                onChange={(e) => setIncludeSpecialChars(e.target.checked)}
-              />
-            }
-            label="Include Special Characters"
-          />
-        </FormGroup>
+        <FormControlLabel
+          control={<Checkbox color='success' checked={includeNumbers} onChange={(e) => setIncludeNumbers(e.target.checked)} />}
+          label="Include Numbers"
+        />
+        <br />
+        <FormControlLabel
+          control={<Checkbox color='success' checked={includeAlphabets} onChange={(e) => setIncludeAlphabets(e.target.checked)} />}
+          label="Include Alphabets"
+        />
+        <br />
+        <FormControlLabel
+          control={<Checkbox color='success' checked={includeSpecialChars} onChange={(e) => setIncludeSpecialChars(e.target.checked)} />}
+          label="Include Special Characters"
+        />
       </div>
-      <Button
-        sx={{ marginTop: 2 }}
-        variant="outlined"
-        onClick={generatePassword}
-      >
+      <Button sx={{ marginTop: 2,borderRadius:50 }} variant="contained" color="success" onClick={generatePassword}>
         Generate Password
       </Button>
       {password && (
         <div>
-          <p>Generated Password: {password}</p>
-          <button onClick={copyToClipboard}>Copy to Clipboard</button>
+          <TextField
+          sx={{marginTop:5}}
+            label="Generated Password"
+            value={password}
+            color='success'
+            InputProps={{
+              readOnly: true,
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={() => copyToClipboard(password)}>
+                    <ContentCopyIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            fullWidth
+            margin="normal"
+          />
         </div>
       )}
-      <Typography sx={{marginTop:5}} variant="h5" component="h5">
-        Password Generator
-      </Typography>
-      <ul>
-      <List>
-        {passwordHistory.map((pwd, index) => (
-          <ListItem key={index}>
-            <ListItemText primary={pwd} />
-          </ListItem>
-        ))}
-      </List>
       
-      </ul>
-    </div>
+      {passwordHistory.length > 0 && (
+        <>
+          <Typography sx={{ marginTop: 4 }} variant="h6" gutterBottom>
+            Last 5 Passwords
+          </Typography>
+          <List>
+            {passwordHistory.map((pwd, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={pwd} />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="copy" onClick={() => copyToClipboard(pwd)}>
+                    <ContentCopyIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))}
+          </List>
+        </>
+      )}
+    </Container>
   );
 };
 
