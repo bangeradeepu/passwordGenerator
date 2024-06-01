c6a287ea943e509a133e65b1d918fd7fe63fa150
bangeradeepu <deepu.program@gmail.com>
2024-06-01 23:37:24 +0530

    razorImpi

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ import {
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const App = () => {
+  const serverAPI = import.meta.env.VITE_API_LINK;
+
   const [includeNumbers, setIncludeNumbers] = useState(true);
   const [includeAlphabets, setIncludeAlphabets] = useState(true);
   const [includeSpecialChars, setIncludeSpecialChars] = useState(true);
@@ -83,6 +85,107 @@ const App = () => {
     }
   }, []);
 
+
+  const amount = 50000;
+  const currency = "INR";
+  const receiptId ="qwsaql";
+  const openPayment = async() => {
+    const response = await fetch (`${serverAPI}/order`,{
+      method:"POST",
+      body : JSON.stringify({
+          amount,
+          currency,
+          receipt : receiptId,
+      }),
+      headers :{
+          "Content-Type":"application/json",
+      },
+  });
+  const order = await response.json();
+  console.log(order);
+  var options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID, // Enter the Key ID generated from the Dashboard
+      amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency,
+      name: "My Website", // Your business name
+      description: "Test Transaction",
+      image: "https://example.com/your_logo",
+      order_id: order.id, // This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      
+      config: {
+          display: {
+              blocks: {
+                  banks: {
+                      name: 'All payment methods',
+                      instruments: [
+                          {
+                              method: 'upi'
+                          },
+                          {
+                              method: 'card'
+                          },
+                          {
+                              method: 'wallet'
+                          },
+                          {
+                              method: 'netbanking'
+                          }
+                      ],
+                  },
+              },
+              sequence: ['block.banks'],
+              preferences: {
+                  show_default_blocks: false,
+              },
+          },
+      },
+      
+      handler: async function (response) {
+          const body = {
+              ...response,
+          };
+  
+          const validateRes = await fetch(
+              `${serverAPI}/order/validate`,
+              {
+                  method: "POST",
+                  body: JSON.stringify(body),
+                  headers: {
+                      "Content-Type": "application/json",
+                  },
+              }
+          );
+          const jsonRes = await validateRes.json();
+          console.log(jsonRes);
+          console.log(jsonRes.orderId);
+          if(jsonRes.msg === 'success'){
+              console.log("Its success");
+          }
+
+      },
+  
+      prefill: {
+          name: "Deepu", // Your customer's name
+          email: "deepu@gmail.com",
+          contact: "9000000000", // Provide the customer's phone number for better conversion rates
+      },
+      notes: {
+          address: "Razorpay Corporate Office",
+      },
+      theme: {
+          color: "#3399cc",
+      },
+  };
+  
+    
+    var rzp1 = new window.Razorpay(options);
+    rzp1.on("payment.failed", function (response) {
+      alert(response.error.code);
+    });
+    rzp1.open();
+    e.preventDefault();
+  }
+
   return (
     <Container maxWidth="sm">
       <Typography variant="h4" gutterBottom>Password Generator</Typography>
@@ -148,6 +251,7 @@ const App = () => {
         </>
       )}
        <div>
+        <Button variant='outlined' color="success" sx={{marginTop:5}} onClick={openPayment}>Make payment</Button>
      <Stack sx={{marginTop:5}}>
      {latitude && longitude ? (
         <Typography>
